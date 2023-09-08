0e3fba4387b55b36d31c6c25734d3969f0987381
Etienne0101 <etiennepichotdamon@gmail.com>
2023-09-08 14:06:57 +0200

    add supabase

---
+++ pages/access/testaccess.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { supabase } from "../../utils/supabaseClient";
+
+export default function TestAccess() {
+    const [loading, setLoading] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        // Check if there's an active session upon loading the component
+        const checkSession = async () => {
+            const { data: session } = await supabase.auth.getSession();
+            if (session) {
+                setLoggedIn(true);
+            }
+            setLoading(false);
+        };
+        checkSession();
+
+        // Listen for authentication state changes
+        const authListener = supabase.auth.onAuthStateChange((event) => {
+            if (event === 'SIGNED_IN') {
+                setLoggedIn(true);
+            } else {
+                setLoggedIn(false);
+            }
+            setLoading(false);
+        });
+
+        // Cleanup: Unsubscribe from the auth changes when component is unmounted
+        return () => {
+            if (authListener && typeof authListener.unsubscribe === 'function') {
+                authListener.unsubscribe();
+            }
+        };
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            {loggedIn ? "You are logged in" : "You need to log in"}
+        </div>
+    );
+}
