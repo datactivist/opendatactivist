248ab2be7728c60bd5d13f4c98f7d85af29620ee
Etienne0101 <etiennepichotdamon@gmail.com>
2023-09-29 14:28:50 +0200

    update doc api and dataposition

--- pages/api/docs.js
+++ pages/api/docs.js
@@ -1,9 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import fetch from 'node-fetch';
+import { supabase } from "../../utils/supabaseClient";
 
 export default async function handler(req, res) {
   const { filename, action } = req.query;
+
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const REPO_PATH = 'datactivist/open-datactivist-private';
+  const BASE_URL = `https://api.github.com/repos/${REPO_PATH}/contents/docs`;
 
   if (action === 'list') {
     try {
@@ -34,14 +40,52 @@ export default async function handler(req, res) {
         'docs',
         `${filename}.md`,
       );
-      const fileContent = fs.readFileSync(filePath, 'utf8');
-      const { data, content } = matter(fileContent);
+  
+      if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const { data, content } = matter(fileContent);
+        res.status(200).json({ metadata: data, content });
+      } else {
+        // If file not found locally, try fetching from GitHub
+        const fileURL = `${BASE_URL}/${filename}.md`;
+        const response = await fetch(fileURL, {
+          headers: {
+            Authorization: `token ${GITHUB_TOKEN}`,
+            Accept: 'application/vnd.github.v3.raw',
+          },
+        });
 
-      res.status(200).json({ metadata: data, content });
+        if (!response.ok) {
+          throw new Error('Failed to fetch from GitHub API');
+        }
+
+        const content = await response.text();
+        const { data, content: parsedContent } = matter(content);
+
+        // Special handling for "pv" files
+        if (filename.endsWith('pv') && data.access === 'datactivist-team') {
+          const user = req.user;  // Assuming you have user in req
+
+          if (user && user.email) {
+            const { data: supabaseData, error } = await supabase
+              .from('docaccess-datactivist-team')
+              .select('email')
+              .eq('email', user.email);
+
+            if (error || !supabaseData.length) {
+              res.status(403).json({ message: 'Access denied.' });
+              return;
+            }
+          } else {
+            res.status(403).json({ message: 'Access denied. No user email found.' });
+            return;
+          }
+        }
+
+        res.status(200).json({ metadata: data, content: parsedContent });
+      }
     } catch (error) {
-      res
-        .status(500)
-        .json({ message: 'Erreur lors de la récupération du contenu.' });
+      res.status(500).json({ message: 'Erreur lors de la récupération du contenu.' });
     }
   }
 }
