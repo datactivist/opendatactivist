0c84d3cec131aa9ab16f7b2e14ceae6d91a65a96
Etienne0101 <etiennepichotdamon@gmail.com>
2023-11-09 11:35:47 +0100

    create tag page

---
+++ pages/tags.js
@@ -0,0 +1,13 @@
+// pages/docs/index.js
+import TagsList from '../components/nav/TagsList';
+
+const DocsPage = () => {
+  return (
+    <div>
+      {/* ... Autres éléments de ta page ... */}
+      <TagsList />
+    </div>
+  );
+};
+
+export default DocsPage;
