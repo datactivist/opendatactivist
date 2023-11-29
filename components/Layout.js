b32e95200a20cfe80880e4518a2b74a2141f254d
Etienne0101 <etiennepichotdamon@gmail.com>
2023-11-29 13:46:37 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,44 +4,18 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Layout.module.css';
-import { supabase } from '../utils/supabaseClient';
-import userIcon from '/public/icons/user.svg'; // Import your user icon
 
 const Layout = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [appBarPosition, setAppBarPosition] = useState('sticky');
   const [lastScrollPos, setLastScrollPos] = useState(0);
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  useEffect(() => {
-    async function checkUserSession() {
-      const { data, error } = await supabase.auth.getUser();
 
-      if (error) {
-        console.error('Error fetching user:', error.message);
-      } else if (data) {
-        setLoggedIn(true);
-      } else {
-        setLoggedIn(false);
-      }
-    }
-
-    checkUserSession();
-  }, []);
-
-  const handleLogout = async () => {
-    await supabase.auth.signOut();
-    setLoggedIn(false);
-  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -117,41 +91,7 @@ const Layout = ({ children }) => {
               <Link href="/docs" passHref>
                 <span className={styles.link}>Contenus ouverts</span>
               </Link>
-              <div
-                className={`${styles.userContainer} dropdown-container`}
-                onClick={toggleDropdown}
-              >
-                <Image src={userIcon} alt="User" width={30} height={30} />
-                <div
-                  className={`${styles.dropdownMenu} ${
-                    dropdownOpen ? styles.dropdownOpen : ''
-                  }`}
-                >
-                  {loggedIn ? (
-                    <>
-                      <Link href="/account/mes-informations" passHref>
-                        <span
-                          className={`${styles.link} ${styles.dropdownItem}`}
-                        >
-                          Mon compte
-                        </span>
-                      </Link>
-                      <div
-                        onClick={handleLogout}
-                        className={`${styles.link} ${styles.dropdownItem}`}
-                      >
-                        Me déconnecter
-                      </div>
-                    </>
-                  ) : (
-                    <Link href="/auth/login" passHref>
-                      <span className={`${styles.link} ${styles.dropdownItem}`}>
-                        Mon compte
-                      </span>
-                    </Link>
-                  )}
-                </div>
-              </div>
+              
             </nav>
           </div>
         </header>
