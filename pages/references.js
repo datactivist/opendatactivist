4da1eeea3fce78517abec51733841cbf76d78b3c
Etienne0101 <etiennepichotdamon@gmail.com>
2024-02-22 17:11:57 +0100

    update references

--- pages/references.js
+++ pages/references.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link'; // Assurez-vous d'importer Link de next/link
+import Link from 'next/link';
 import styles from '../styles/ReferencesPage.module.css';
 import Layout from '../components/Layout';
 
 const ReferencesPage = () => {
   const [references, setReferences] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [uniqueTypes, setUniqueTypes] = useState([]);
+  const [selectedType, setSelectedType] = useState(null);
 
   useEffect(() => {
     fetch('/api/references?action=list')
       .then(response => response.json())
       .then(data => {
+        const types = [...new Set(data.flatMap(reference => reference.type))];
+        setUniqueTypes(types);
         setReferences(data);
         setIsLoading(false);
       })
@@ -20,6 +24,14 @@ const ReferencesPage = () => {
       });
   }, []);
 
+  const toggleFilter = (type) => {
+    setSelectedType(selectedType === type ? null : type);
+  };
+
+  const filteredReferences = selectedType
+    ? references.filter(reference => reference.type.includes(selectedType))
+    : references;
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -27,8 +39,19 @@ const ReferencesPage = () => {
   return (
     <Layout>
       <h1 className={styles.referencePageTitle}>Nos références</h1>
+      <div className={styles.buttonContainer}> {/* Use buttonContainer for the div containing the buttons */}
+        {uniqueTypes.map((type, index) => (
+          <button
+            key={index}
+            onClick={() => toggleFilter(type)}
+            className={selectedType === type ? `${styles.filterButton} ${styles.activeFilterButton}` : styles.filterButton} /* Use filterButton and conditionally activeFilterButton */
+          >
+            {type}
+          </button>
+        ))}
+      </div>
       <div className={styles.referencesContainer}>
-        {references.map((reference) => (
+        {filteredReferences.map((reference) => (
           <Link key={reference.id} href={`/references/${reference.id}`} passHref>
             <div className={styles.referenceCard}>
               <h2 className={styles.referenceTitle}>{reference.title}</h2>
@@ -44,6 +67,6 @@ const ReferencesPage = () => {
       </div>
     </Layout>
   );
-};
+  };
 
 export default ReferencesPage;
