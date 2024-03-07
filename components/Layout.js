84aeaa5f3f7c7089af93e8542ec4f8d6866b0233
Etienne0101 <etiennepichotdamon@gmail.com>
2024-03-07 10:31:27 +0100

    update layout and main pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,172 +1,65 @@
 // components/Layout.js
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import styles from '../styles/Layout.module.css';
+import Image from 'next/image'; // Importez Image de Next.js pour une meilleure optimisation des images
+import styles from '../styles/Layout.module.css'; // Assurez-vous de créer ce fichier CSS
 
 const Layout = ({ children }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [appBarPosition, setAppBarPosition] = useState('sticky');
-  const [lastScrollPos, setLastScrollPos] = useState(0);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [visible, setVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(0);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const updateDimensions = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    setVisible(currentScrollY < lastScrollY || currentScrollY === 0);
+    setLastScrollY(currentScrollY);
   };
 
   useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      if (currentScrollPos < lastScrollPos) {
-        setAppBarPosition('sticky');
-      } else {
-        setAppBarPosition('relative');
-      }
-      setLastScrollPos(currentScrollPos);
-    };
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener('resize', updateDimensions);
 
-    window.addEventListener('scroll', handleScroll);
+    updateDimensions();
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', updateDimensions);
     };
-  }, [lastScrollPos]);
-
-  useEffect(() => {
-    const closeDropdown = (event) => {
-      if (dropdownOpen && !event.target.closest('.dropdown-container')) {
-        setDropdownOpen(false);
-      }
-    };
-
-    window.addEventListener('click', closeDropdown);
-
-    return () => {
-      window.removeEventListener('click', closeDropdown);
-    };
-  }, [dropdownOpen]);
+  }, [lastScrollY]);
 
   return (
-    <div>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div style={{ width: '90%', margin: '0 auto' }}>
-        <header className={`${styles.appBar} ${styles[appBarPosition]}`}>
-          <div className={styles.toolbar}>
-            <button onClick={toggleMenu} className={styles.hamburgerButton}>
-              ☰
-            </button>
-            <div className={styles.titleContainer}>
-              <a href="/" className={styles.title}>
-                open{' '}
-              </a>
-              <Link href="/" passHref>
-                <Image
-                  src="/images/footer/logo-datactivist.png"
-                  alt="Datactivist logo"
-                  width={220}
-                  height={220}
-                  priority
-                  className={styles.logo}
-                />
-              </Link>
-            </div>
-            <nav
-              className={`${styles.navLinks} ${
-                menuOpen ? styles.showMenu : ''
-              }`}
-            >
-              <Link href="/products" passHref>
-                <span className={styles.link}>Outils & produits</span>
-              </Link>
-              <Link href="/docs" passHref>
-                <span className={styles.link}>Publications</span>
-              </Link>
-              <Link href="/blog" passHref>
-                <span className={styles.link}>✏ Blog</span>
-              </Link>
-            </nav>
-          </div>
-        </header>
-      </div>
-      <main className={styles.layout}>
-        <div className={`container ${styles.container}`}>
-          <div className={styles.box}>{children}</div>
+    <>
+      <header className={`${styles.header} ${visible ? styles.visible : styles.hidden}`}>
+        <div className={styles.logoContainer}>
+          <Image className={styles.logoDatactivist} src="/images/footer/logo-datactivist.png" alt="Datactivist Logo" width={100} height={50} />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <div className={styles.footerBox}>
-          <a
-            href="https://twitter.com/datactivi_st"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ marginRight: '40px' }}
-          >
-            <Image
-              src="/images/footer/twitter.svg"
-              alt="Twitter"
-              width={30}
-              height={30}
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/datactivist"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ marginRight: '40px' }}
-          >
-            <Image
-              src="/images/footer/linkedin.svg"
-              alt="LinkedIn"
-              width={30}
-              height={30}
-            />
-          </a>
-          <a
-            href="https://medium.com/datactivist"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ marginRight: '40px' }}
-          >
-            <Image
-              src="/images/footer/medium.svg"
-              alt="Medium"
-              width={30}
-              height={30}
-            />
-          </a>
-          <a
-            href="mailto:hello@datactivist.coop"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ marginRight: '40px' }}
-          >
-            <Image
-              src="/images/footer/mail.svg"
-              alt="Email"
-              width={30}
-              height={30}
-            />
-          </a>
-          <div>
-            <hr></hr>
-            <Link href="/partners">
-              <span className={styles.footerLink}>🤍 Partenaires</span>
-            </Link>
-            <Link href="/links">
-              <span className={styles.footerLink}>🔗 Ressources externes</span>
-            </Link>
-            <Link href="/stats">
-              <span className={styles.footerLink}>📊 Stats</span>
-            </Link>
-          </div>
-        </div>
-      </footer>
-    </div>
+        {windowWidth > 768 ? (
+          <nav className={styles.menunav}>
+            <Link href="/docs">Publications</Link>
+            <Link href="/products">Produits</Link>
+            <Link href="/equipe">Équipe</Link>
+            <Link href="/references">Références</Link>
+            <Link href="/recherche">Recherche</Link>
+            <Link href="/blog">Blog</Link>
+          </nav>
+        ) : (
+          <select className={styles.menuselect} onChange={(e) => window.location.href = e.target.value}>
+            <option value="">Menu</option>
+            <option value="/docs">Publications</option>
+            <option value="/products">Produits</option>
+            <option value="/equipe">Equipe</option>
+            <option value="/references">Références</option>
+            <option value="/recherche">Recherche</option>
+            <option value="/blog">Blog</option>
+          </select>
+        )}
+      </header>
+      <main className={styles.mainContent}>{children}</main>
+    </>
   );
 };
 
