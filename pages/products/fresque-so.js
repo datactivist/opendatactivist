c07f9bf11962e3c9c53684d4d8e36cb0be2769de
AnthonyGigerich <anthony@datactivist.coop>
2026-01-13 14:29:12 +0100

    attempt fix fresque-so

--- pages/products/fresque-so.js
+++ pages/products/fresque-so.js
@@ -1,10 +1,11 @@
-import ProductPage from "../../components/products/ProductPage"
-import Layout from "../../components/Layout"
+import ProductPage from '../../components/products/ProductPage';
+import Layout from '../../components/Layout';
 export default function Produit() {
   const produit = {
     docsList: ['fresque-so'],
     nom: 'Fresque de la Science Ouverte',
-    baseline: 'Une fresque pour comprendre les enjeux et les concepts de la science ouverte',
+    baseline:
+      'Une fresque pour comprendre les enjeux et les concepts de la science ouverte',
     description: `
     <h3>Découvrez les concepts et les enjeux de la science ouverte </h3>
     La fresque de la science ouverte a été développée en collaboration avec les équipes de la mission science ouverte de l'IRD. Elle vise à comprendre et mettre en perspectives les principales notions de la science ouverte.
@@ -17,20 +18,29 @@ export default function Produit() {
 
     `,
     imageUrl: '/images/products/fresque-so.png',
-    targets: ['🦊 Curieuses et curieux','👩‍🏫 Elèves, étudiants','🧑🏻‍💻 Agents publics'],
-    partnersIds: ['ird'],
-    
+    targets: [
+      '🦊 Curieuses et curieux',
+      '👩‍🏫 Elèves, étudiants',
+      '🧑🏻‍💻 Agents publics',
     ],
+    partnersIds: ['ird'],
+
     liens: [
-        {url: 'mailto:hello@datactivist.coop?subject=Demande d\'informations - fresque science ouverte', texte: '✉️ Nous contacter pour en savoir plus'},
-        {url: 'https://open.datactivist.coop/docs/cartes-fresque-so', texte: '🖨️ Imprimez le jeu'}
-      ]
-  }
+      {
+        url: "mailto:hello@datactivist.coop?subject=Demande d'informations - fresque science ouverte",
+        texte: '✉️ Nous contacter pour en savoir plus',
+      },
+      {
+        url: 'https://open.datactivist.coop/docs/cartes-fresque-so',
+        texte: '🖨️ Imprimez le jeu',
+      },
+    ],
+  };
 
   return (
     <Layout>
-    <br></br>
-    <ProductPage {...produit} />
+      <br></br>
+      <ProductPage {...produit} />
     </Layout>
-  )
+  );
 }
