670a6e98582afa242b31afe7282eb4b1f9908827
EliseHPC <105201026+EliseHPC@users.noreply.github.com>
2026-04-28 15:11:50 +0200

    Update utilities.js
    corrections coquilles

--- pages/products/utilities.js
+++ pages/products/utilities.js
@@ -6,7 +6,7 @@ export default function Produit() {
     nom: 'Données des réseaux et utilities',
     baseline: 'Notre experience au service des gestionnaires de réseaux, autorités organisatrices et services publics en réseau',
     description: `Nous produisons, traitons les données et conseillons nos clients dans leurs usages au service des infrastructures et des services publics en réseaux.
-    Nous avons rassemblés les retours d'expérience les plus significatifs ci-dessous autour de thèmes précis :
+    Nous avons rassemblé les retours d'expérience les plus significatifs ci-dessous autour de quatre thèmes :
     <ul>
       <li>La transition énergétique</li>
       <li>La gestion des immobilisations et du patrimoine des infrastructures</li>
@@ -14,7 +14,7 @@ export default function Produit() {
       <li>Le remplacement des télécommunications en cuivre par la fibre optique</li>
     </ul>
 
-    Notre exprience s'établit entre des outils conçus puis maintenus sur ces sujets et au gré des missions pour nos clients dont nous avons pu publier certaines ressources pour notre propre compte.
+    Notre expérience s'établit entre des outils conçus puis maintenus sur ces sujets et au gré des missions pour nos clients, dont nous avons pu publier certaines ressources pour notre propre compte.
     `,
     imageUrl: '/images/products/utilities.png',
     targets: [
