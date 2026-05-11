52e87452d36196ec2d8c8e0c20f952499df0ab96
FLacombe <francois.lacombe@infos-reseaux.com>
2026-05-11 14:15:45 +0200

    Update openstreetmap.js
    Publish openstreetmap-monitoring-contribution

--- pages/products/openstreetmap.js
+++ pages/products/openstreetmap.js
@@ -2,7 +2,7 @@ import ProductPage from "../../components/products/ProductPage"
 import Layout from "../../components/Layout"
 export default function Produit() {
   const produit = {
-    docsList: ['openstreetmap-presentation-generale', 'tout-savoir-licence-odbl', 'openstreetmap-acceder-donnees', 'openstreetmap-chez-datactivist', 'openstreetmap-confiance-qualite', 'openstreetmap-nature-ville', 'openstreetmap-ontologie'],
+    docsList: ['openstreetmap-presentation-generale', 'tout-savoir-licence-odbl', 'openstreetmap-acceder-donnees', 'openstreetmap-chez-datactivist', 'openstreetmap-confiance-qualite', 'openstreetmap-monitoring-contribution', 'openstreetmap-nature-ville', 'openstreetmap-ontologie'],
     nom: 'OpenStreetMap : utiliser et contribuer à la base de données collaborative',
     baseline: 'Découvrez nos ressources à propos du projet mondial de cartographie, pour vous aussi vous lancer dans la contribution à ce commun numérique',
     description: `<strong>Découvrez nos expertises et ressources à propos du projet OpenStreetMap</strong>
